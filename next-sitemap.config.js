bdb8c952d88915ccb480d0769ebe2079ac222157
Sheikh A. Mannan <45845727+sabdulm@users.noreply.github.com>
2022-09-18 23:25:42 -0600

    removed comments

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
-// Default code you can customize according to your requirements.
+
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://razakhawaja.me',
-    generateRobotsTxt: true, // (optional)
+    generateRobotsTxt: true,
     generateIndexSitemap: false,
-    // REST CODE READ DOCS  ...
-    }
+}
