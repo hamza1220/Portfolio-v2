05fbcbab59cced6921429680bf0561f5452c2f51
Sheikh A. Mannan <45845727+sabdulm@users.noreply.github.com>
2022-09-18 23:03:20 -0600

    added sitemap functionality

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+// Default code you can customize according to your requirements.
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://razakhawaja.me',
+    generateRobotsTxt: true, // (optional)
+    generateIndexSitemap: false,
+    // REST CODE READ DOCS  ...
+    }
