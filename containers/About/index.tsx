718336a7789ba1b8d1ec841f219ba3ac0ec31081
Raza Khawaja <razamufc@gmail.com>
2023-02-05 16:53:41 -0500

    About Page Updates

--- containers/About/index.tsx
+++ containers/About/index.tsx
@@ -8,7 +8,7 @@ const About = ({ about }: { about: string }) => (
   <div id="about" className="w-full py-24 flex flex-col justify-center ">
     <SectionHeading text={"About Me"} index={1} />
     <div className="flex flex-col-reverse lg:grid lg:grid-cols-1 lg:gap-4">
-      <div className="sm:col-start-1 sm:col-end-4 leading-normal text-xl text-blueGray-500 dark:text-gray-600 about-container">
+      <div className="sm:col-start-1 sm:col-end-4 leading-normal text-blueGray-500 dark:text-gray-600 about-container">
         <Markdown content={about} isNotFullPage />
       </div>
       <div className="flex justify-center items-center mb-8 sm:mt-0 sm:col-end-6 sm:col-span-2 ">
